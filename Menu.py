2e33121aedf25e323feee24162c2bf4166c5f7fd
olekern <ole.ekern@gmail.com>
2019-01-08 22:45:21 +0100

    Menu design (still shait)

--- Menu.py
+++ Menu.py
@@ -10,7 +10,7 @@ class Menu:
         levelPaths = ["maps/Map1.json", "maps/Map2.json", "maps/Map3.json", "maps/Map4.json"]
         for i in range(game.LEVEL_COUNT):
             x_pos = (game.SCREEN_WIDTH/6)*(i)+(game.SCREEN_WIDTH/12)*(i+0.5)
-            button = Button(self, game, x_pos, game.SCREEN_HEIGHT/2, levelPaths[i])
+            button = Button(self, game, x_pos, game.SCREEN_HEIGHT/2, levelPaths[i], i)
             self.buttons.append(button)
 
     def update(self, screen, events):
@@ -35,7 +35,7 @@ class Menu:
             self.buttons[i].draw(screen)
 
 class Button:
-    def __init__(self, menu, game, x, y, levelPath):
+    def __init__(self, menu, game, x, y, levelPath, i):
         self.menu = menu
         self.game = game
         self.x = x
@@ -43,9 +43,14 @@ class Button:
         self.width = 150
         self.height = 65
         self.levelPath = levelPath
+        self.i = i+1
 
     def draw(self, screen):
         pygame.draw.rect(screen, (254, 198, 1), (self.x, self.y, self.game.SCREEN_WIDTH/6, 65)) 
+        print(self.i)
+        text = self.game.font.render('Level ' + str(self.i), False, (255, 255, 255))
+        textWidth = text.get_rect().width
+        screen.blit(text, (self.x + (self.width-textWidth)/3, self.y + 7))
 
     def collide(self, mouse):
         x_pos = mouse[0]
