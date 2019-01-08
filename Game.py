bcb797fc0eb380521049abb6ad16605efaee26c6
Kristoffer Lundquist <lunke@hotmail.no>
2019-01-08 10:45:21 +0100

    Alt i klasser

--- Game.py
+++ Game.py
@@ -3,30 +3,6 @@ import json
 import random
 from pygame.locals import *
 
-
-TILE_SIZE = 32
-GRAVITY = 80
-WALK_SPEED = 0.7
-JUMP_FORCE = -15
-DAMPING_STOP = 0.8
-DAMPING_TURNING = 0.6
-DAMPING_RUNNING = 0.4
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-pygame.display.set_caption("Beste spillet")
-clock = pygame.time.Clock()
-
-pygame.font.init()
-font = pygame.font.SysFont('Arial', 40)
-
-
 class SpriteSheet:
     def __init__(self, filename, spriteWidth, spriteHeight):
         self.spriteSheet = pygame.image.load(filename).convert_alpha()
@@ -55,16 +31,17 @@ class Camera(object):
         self.y = 0
     
     def update(self):
-        self.x = self.player.x - SCREEN_WIDTH / 2
-        self.y = self.player.y - SCREEN_HEIGHT / 2
+        game = self.player.level.game
+        self.x = self.player.x - game.SCREEN_WIDTH / 2
+        self.y = self.player.y - game.SCREEN_HEIGHT / 2
         
         if self.x < 0:
             self.x = 0
-        elif self.x > len(tiles[0])*TILE_SIZE - SCREEN_WIDTH:
-            self.x = len(tiles[0])*TILE_SIZE - SCREEN_WIDTH
+        elif self.x > len(game.level.tiles[0])*game.TILE_SIZE - game.SCREEN_WIDTH:
+            self.x = len(game.level.tiles[0])*game.TILE_SIZE - game.SCREEN_WIDTH
         
-        if self.y > len(tiles)*TILE_SIZE - SCREEN_HEIGHT:
-            self.y = len(tiles)*TILE_SIZE - SCREEN_HEIGHT
+        if self.y > len(game.level.tiles)*game.TILE_SIZE - game.SCREEN_HEIGHT:
+            self.y = len(game.level.tiles)*game.TILE_SIZE - game.SCREEN_HEIGHT
 
 class MapLoader:
     def __init__(self, path):
@@ -88,21 +65,28 @@ class MapLoader:
         return tiles
 
 class Tile:
-    def __init__(self, id, image):
+    def __init__(self, game, id, image):
+        self.game = game
         self.id = id
         self.image = image
 
     def draw(self, screen, x, y):
-        screen.blit(self.image, (x*TILE_SIZE - camera.x, y*TILE_SIZE - camera.y))
+        screen.blit(self.image, (x*self.game.TILE_SIZE - self.game.level.camera.x, y*self.game.TILE_SIZE - self.game.level.camera.y))
     
 class Player:
-    def __init__(self, x, y):
+    def __init__(self, level, x, y):
+        self.level = level
         self.startX = x
         self.startY = y
         self.x = x
         self.y = y
         self.w = 24
         self.h = 40
+        self.WALK_SPEED = 100
+        self.JUMP_FORCE = -15
+        self.DAMPING_STOP = 0.8
+        self.DAMPING_TURNING = 0.6
+        self.DAMPING_RUNNING = 0.4
 
         self.spriteSheet = SpriteSheet("res/player.png", self.w, self.h)
         self.idle_sprites_right = self.spriteSheet.getImages(0)
@@ -146,24 +130,25 @@ class Player:
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             dx += 1
         
-        self.vx += dx * WALK_SPEED
-        if dx == 0:
+        self.vx = dx * self.WALK_SPEED
+        """if dx == 0:
             self.vx *= (1-DAMPING_STOP)**(dt*10)
         elif self.vx/dx < 0:
             self.vx *= (1-DAMPING_TURNING)**(dt*10)
         else:
-            self.vx *= (1-DAMPING_RUNNING)**(dt*10)
+            self.vx *= (1-DAMPING_RUNNING)**(dt*10)"""
+        
 
         if abs(self.vx) < 0.1:
             self.vx = 0
 
-        self.vy += GRAVITY * dt
+        self.vy += self.level.GRAVITY * dt
         if not self.grounded:
             self.timeInAir += dt
         
 
         if keys[pygame.K_SPACE] and self.timeInAir < 0.03:
-            self.vy = JUMP_FORCE
+            self.vy = self.JUMP_FORCE
         
         for event in events:
             if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
@@ -182,10 +167,10 @@ class Player:
 
         if self.x < 0:
             self.x = 0
-        if self.x > len(tiles[0])*TILE_SIZE - player.w:
-            self.x = len(tiles[0])*TILE_SIZE - player.w
+        if self.x > len(self.level.tiles[0])*self.level.game.TILE_SIZE - self.w:
+            self.x = len(self.level.tiles[0])*self.level.game.TILE_SIZE - self.w
         
-        if self.y>len(tiles)*TILE_SIZE:
+        if self.y>len(self.level.tiles)*self.level.game.TILE_SIZE:
             self.x = self.startX
             self.y = self.startY
             self.vx = 0
@@ -197,41 +182,41 @@ class Player:
             yCheck = self.y + self.h + self.vy
             xCheck1 = self.x
             xCheck2 = self.x + self.w - 1
-            tile1 = getTile(int(xCheck1 / TILE_SIZE), int(yCheck / TILE_SIZE), tiles)
-            tile2 = getTile(int(xCheck2 / TILE_SIZE), int(yCheck / TILE_SIZE), tiles)
+            tile1 = getTile(int(xCheck1 / self.level.game.TILE_SIZE), int(yCheck / self.level.game.TILE_SIZE), tiles)
+            tile2 = getTile(int(xCheck2 / self.level.game.TILE_SIZE), int(yCheck / self.level.game.TILE_SIZE), tiles)
             if (tile1 is not 0 and tile1 is not None) or (tile2 is not 0 and tile2 is not None):
                 self.timeInAir = 0
                 self.grounded = True
                 self.vy = 0
-                self.y = int(yCheck / TILE_SIZE)*TILE_SIZE - self.h
+                self.y = int(yCheck / self.level.game.TILE_SIZE)*self.level.game.TILE_SIZE - self.h
         elif self.vy < 0:
             yCheck = self.y + self.vy
             xCheck1 = self.x
             xCheck2 = self.x + self.w - 1
-            tile1 = getTile(int(xCheck1 / TILE_SIZE), int(yCheck / TILE_SIZE), tiles)
-            tile2 = getTile(int(xCheck2 / TILE_SIZE), int(yCheck / TILE_SIZE), tiles)
+            tile1 = getTile(int(xCheck1 / self.level.game.TILE_SIZE), int(yCheck / self.level.game.TILE_SIZE), tiles)
+            tile2 = getTile(int(xCheck2 / self.level.game.TILE_SIZE), int(yCheck / self.level.game.TILE_SIZE), tiles)
             if (tile1 is not 0 and tile1 is not None) or (tile2 is not 0 and tile2 is not None):
                 self.vy = 0
-                self.y = int(yCheck / TILE_SIZE + 1)*TILE_SIZE
+                self.y = int(yCheck / self.level.game.TILE_SIZE + 1)*self.level.game.TILE_SIZE
         
         if self.vx > 0:
             xCheck = self.x + self.w + self.vx
             yCheck1 = self.y
             yCheck2 = self.y + self.h - 1
-            tile1 = getTile(int(xCheck / TILE_SIZE), int(yCheck1 / TILE_SIZE), tiles)
-            tile2 = getTile(int(xCheck / TILE_SIZE), int(yCheck2 / TILE_SIZE), tiles)
+            tile1 = getTile(int(xCheck / self.level.game.TILE_SIZE), int(yCheck1 / self.level.game.TILE_SIZE), tiles)
+            tile2 = getTile(int(xCheck / self.level.game.TILE_SIZE), int(yCheck2 / self.level.game.TILE_SIZE), tiles)
             if (tile1 is not 0 and tile1 is not None) or (tile2 is not 0 and tile2 is not None):
                 self.vx = 0
-                self.x = int(xCheck / TILE_SIZE)*TILE_SIZE - self.w
+                self.x = int(xCheck / self.level.game.TILE_SIZE)*self.level.game.TILE_SIZE - self.w
         elif self.vx < 0:
             xCheck = self.x + self.vx
             yCheck1 = self.y
             yCheck2 = self.y + self.h - 1
-            tile1 = getTile(int(xCheck / TILE_SIZE), int(yCheck1 / TILE_SIZE), tiles)
-            tile2 = getTile(int(xCheck / TILE_SIZE), int(yCheck2 / TILE_SIZE), tiles)
+            tile1 = getTile(int(xCheck / self.level.game.TILE_SIZE), int(yCheck1 / self.level.game.TILE_SIZE), tiles)
+            tile2 = getTile(int(xCheck / self.level.game.TILE_SIZE), int(yCheck2 / self.level.game.TILE_SIZE), tiles)
             if (tile1 is not 0 and tile1 is not None) or (tile2 is not 0 and tile2 is not None):
                 self.vx = 0
-                self.x = int(xCheck / TILE_SIZE + 1)*TILE_SIZE
+                self.x = int(xCheck / self.level.game.TILE_SIZE + 1)*self.level.game.TILE_SIZE
         
 
  
@@ -256,10 +241,11 @@ class Player:
         else:
             sprite = self.idle_sprites_left[self.spriteIndex]
             
-        screen.blit(sprite, (self.x - camera.x, self.y - camera.y))
+        screen.blit(sprite, (self.x - self.level.camera.x, self.y - self.level.camera.y))
             
 class Cloud:
-    def __init__(self, x, y, sprite, speed):
+    def __init__(self, level, x, y, sprite, speed):
+        self.level = level
         self.x = x
         self.y = y
         self.sprite = sprite
@@ -268,12 +254,12 @@ class Cloud:
     def update(self, dt):
         self.x -= self.speed*dt
         if self.x < -self.sprite.get_rect().width:
-            self.x = SCREEN_WIDTH + random.randint(0, 100)
-            self.y = random.randint(0, SCREEN_HEIGHT/3)
+            self.x = self.level.game.SCREEN_WIDTH + random.randint(0, 100)
+            self.y = random.randint(0, self.level.game.SCREEN_HEIGHT/3)
             self.speed = random.randint(10, 50)
 
     def draw(self, screen):
-        screen.blit(self.sprite, (self.x - camera.x/50, self.y - camera.y/50))
+        screen.blit(self.sprite, (self.x - self.level.camera.x/50, self.y - self.level.camera.y/50))
 
     
 def getTile(x, y, group):
@@ -290,122 +276,137 @@ def getTileWithId(id, group):
     return
 
 
-spriteSheet = SpriteSheet("res/sprites.png", TILE_SIZE, TILE_SIZE)
+class Game:
+    def __init__(self):
+        self.TILE_SIZE = 32
+        self.GREEN = (0, 255, 0)
+        self.BLUE = (0, 0, 255)
 
-TILE_GRASS_TOP = Tile(2, spriteSheet.getImageAt(1, 0))
-TILE_GRASS_TOP_LEFT = Tile(1, spriteSheet.getImageAt(0, 0))
-TILE_GRASS_TOP_RIGHT = Tile(3, spriteSheet.getImageAt(2, 0))
-TILE_GRASS_LEFT = Tile(9, spriteSheet.getImageAt(0, 1))
-TILE_DIRT = Tile(10, spriteSheet.getImageAt(1,1))
-TILE_GRASS_RIGHT = Tile(11, spriteSheet.getImageAt(2, 1))
+        self.SCREEN_WIDTH = 800
+        self.SCREEN_HEIGHT = 600
 
-ALL_TILES = [TILE_GRASS_TOP, TILE_GRASS_TOP_LEFT, TILE_GRASS_TOP_RIGHT, TILE_GRASS_LEFT, TILE_DIRT, TILE_GRASS_RIGHT]
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        #screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        pygame.display.set_caption("Beste spillet")
+        self.clock = pygame.time.Clock()
 
-DECORATION_GRASS = Tile(4, spriteSheet.getImageAt(3, 0))
-DECORATION_SIGN_5 = Tile(5, spriteSheet.getImageAt(4, 0))
-DECORATION_SIGN_6 = Tile(6, spriteSheet.getImageAt(5, 0))
-DECORATION_SIGN_7 = Tile(7, spriteSheet.getImageAt(6, 0))
-DECORATION_SIGN_8 = Tile(8, spriteSheet.getImageAt(7, 0))
-DECORATION_SIGN_13 = Tile(13, spriteSheet.getImageAt(4, 1))
-DECORATION_SIGN_14 = Tile(14, spriteSheet.getImageAt(5, 1))
-DECORATION_SIGN_15 = Tile(15, spriteSheet.getImageAt(6, 1))
-DECORATION_SIGN_16 = Tile(16, spriteSheet.getImageAt(7, 1))
-DECORATION_SIGN_21 = Tile(21, spriteSheet.getImageAt(4, 2))
-DECORATION_SIGN_22 = Tile(22, spriteSheet.getImageAt(5, 2))
-DECORATION_GOAL_17 = Tile(17, spriteSheet.getImageAt(0, 2))
-DECORATION_GOAL_25 = Tile(25, spriteSheet.getImageAt(0, 3))
+        pygame.font.init()
+        self.font = pygame.font.SysFont('Arial', 40)
+
+        spriteSheet = SpriteSheet("res/sprites.png", self.TILE_SIZE, self.TILE_SIZE)
+
+        self.TILE_GRASS_TOP = Tile(self, 2, spriteSheet.getImageAt(1, 0))
+        self.TILE_GRASS_TOP_LEFT = Tile(self, 1, spriteSheet.getImageAt(0, 0))
+        self.TILE_GRASS_TOP_RIGHT = Tile(self, 3, spriteSheet.getImageAt(2, 0))
+        self.TILE_GRASS_LEFT = Tile(self, 9, spriteSheet.getImageAt(0, 1))
+        self.TILE_DIRT = Tile(self, 10, spriteSheet.getImageAt(1,1))
+        self.TILE_GRASS_RIGHT = Tile(self, 11, spriteSheet.getImageAt(2, 1))
+
+        self.ALL_TILES = [self.TILE_GRASS_TOP, self.TILE_GRASS_TOP_LEFT, self.TILE_GRASS_TOP_RIGHT, self.TILE_GRASS_LEFT, self.TILE_DIRT, self.TILE_GRASS_RIGHT]
+
+        self.DECORATION_GRASS = Tile(self, 4, spriteSheet.getImageAt(3, 0))
+        self.DECORATION_SIGN_5 = Tile(self, 5, spriteSheet.getImageAt(4, 0))
+        self.DECORATION_SIGN_6 = Tile(self, 6, spriteSheet.getImageAt(5, 0))
+        self.DECORATION_SIGN_7 = Tile(self, 7, spriteSheet.getImageAt(6, 0))
+        self.DECORATION_SIGN_8 = Tile(self, 8, spriteSheet.getImageAt(7, 0))
+        self.DECORATION_SIGN_13 = Tile(self, 13, spriteSheet.getImageAt(4, 1))
+        self.DECORATION_SIGN_14 = Tile(self, 14, spriteSheet.getImageAt(5, 1))
+        self.DECORATION_SIGN_15 = Tile(self, 15, spriteSheet.getImageAt(6, 1))
+        self.DECORATION_SIGN_16 = Tile(self, 16, spriteSheet.getImageAt(7, 1))
+        self.DECORATION_SIGN_21 = Tile(self, 21, spriteSheet.getImageAt(4, 2))
+        self.DECORATION_SIGN_22 = Tile(self, 22, spriteSheet.getImageAt(5, 2))
+        self.DECORATION_GOAL_17 = Tile(self, 17, spriteSheet.getImageAt(0, 2))
+        self.DECORATION_GOAL_25 = Tile(self, 25, spriteSheet.getImageAt(0, 3))
 
 
-ALL_DECORATIONS = [DECORATION_GRASS, DECORATION_SIGN_5, DECORATION_SIGN_6, DECORATION_SIGN_7, DECORATION_SIGN_8, DECORATION_SIGN_13, DECORATION_SIGN_14, DECORATION_SIGN_15, DECORATION_SIGN_16, DECORATION_SIGN_21, DECORATION_SIGN_22, DECORATION_GOAL_17, DECORATION_GOAL_25]
+        self.ALL_DECORATIONS = [self.DECORATION_GRASS, self.DECORATION_SIGN_5, self.DECORATION_SIGN_6, self.DECORATION_SIGN_7, self.DECORATION_SIGN_8, self.DECORATION_SIGN_13, self.DECORATION_SIGN_14, self.DECORATION_SIGN_15, self.DECORATION_SIGN_16, self.DECORATION_SIGN_21, self.DECORATION_SIGN_22, self.DECORATION_GOAL_17, self.DECORATION_GOAL_25]
+        self.level = Level(self, "maps/Map1.json")
 
-player = Player(TILE_SIZE*2, 0)
-camera = Camera(player)
 
-sun = pygame.image.load("res/sun.png").convert_alpha()
+    def run(self):
+        running = True
+        while running:
+            dt = self.clock.tick(60) / 1000
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
 
-running = True
+            self.level.update(self.screen, events, dt)
 
-map1 = MapLoader("maps/Map1.json")
-tiles = map1.getTiles("Ground")
-decorations = map1.getTiles("Decoration")
+            pygame.display.flip()
 
-cloudImg = pygame.image.load("res/cloud.png").convert_alpha()
-clouds = []
 
-for i in range(5):
-    x = random.randint(0, SCREEN_WIDTH)
-    y = random.randint(0, SCREEN_HEIGHT/3)
-    speed = random.randint(10, 50)
-    clouds.append(Cloud(x, y, cloudImg, speed))
+        pygame.quit()
 
-while running:
-    dt = clock.tick(60) / 1000
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
 
-    player.update(dt, events)
-    player.checkCollisions(tiles)
-    player.lateUpdate()
 
-    camera.update()
+class Level:
+    def __init__(self, game, mapRef):
+        self.game = game
+        self.mapRef = mapRef
+        self.player = Player(self, game.TILE_SIZE*2, 0)
+        self.camera = Camera(self.player)
+        self.sun = pygame.image.load("res/sun.png").convert_alpha()
+        self.map1 = MapLoader(mapRef)
+        self.tiles = self.map1.getTiles("Ground")
+        self.decorations = self.map1.getTiles("Decoration")
+        self.GRAVITY = 80
 
-    screen.fill((95, 187, 210))
-    screen.blit(sun, (SCREEN_WIDTH-600, 0))
-
-    for cloud in clouds:
-        cloud.update(dt)
-        cloud.draw(screen)
+        cloudImg = pygame.image.load("res/cloud.png").convert_alpha()
+        self.clouds = []
+        for i in range(5):
+            x = random.randint(0, self.game.SCREEN_WIDTH)
+            y = random.randint(0, self.game.SCREEN_HEIGHT/3)
+            speed = random.randint(10, 50)
+            self.clouds.append(Cloud(self, x, y, cloudImg, speed))
     
-    minY = int((camera.y) / TILE_SIZE)
-    maxY = int((camera.y + SCREEN_HEIGHT) / TILE_SIZE) + 1
-    minX = int((camera.x) / TILE_SIZE)
-    maxX = int((camera.x + SCREEN_WIDTH) / TILE_SIZE) + 1
-    for y in range(minY, maxY):
-        for x in range(minX, maxX):
-            tileID = getTile(x, y, tiles)
-            if tileID is not None:
-                tile = getTileWithId(tileID, ALL_TILES)
-                if tile is not None:
-                    tile.draw(screen, x, y)
-            decorID = getTile(x, y, decorations)
-            if decorID is not None:
-                decor = getTileWithId(decorID, ALL_DECORATIONS)
-                if decor is not None:
-                    decor.draw(screen, x, y)
+    def update(self, screen, events, dt):
+        self.player.update(dt, events)
+        self.player.checkCollisions(self.tiles)
+        self.player.lateUpdate()
 
-    
+        self.camera.update()
 
-    player.draw(screen)
+        screen.fill((95, 187, 210))
+        screen.blit(self.sun, (self.game.SCREEN_WIDTH-600, 0))
 
-    time = pygame.time.get_ticks() / 1000
-    text = font.render('Tid: ' + str(time), True, (255, 255, 255))
-    screen.blit(text, (10, 10))
+        for cloud in self.clouds:
+            cloud.update(dt)
+            cloud.draw(screen)
+        
+        minY = int((self.camera.y) / self.game.TILE_SIZE)
+        maxY = int((self.camera.y + self.game.SCREEN_HEIGHT) / self.game.TILE_SIZE) + 1
+        minX = int((self.camera.x) / self.game.TILE_SIZE)
+        maxX = int((self.camera.x + self.game.SCREEN_WIDTH) / self.game.TILE_SIZE) + 1
+        for y in range(minY, maxY):
+            for x in range(minX, maxX):
+                tileID = getTile(x, y, self.tiles)
+                if tileID is not None:
+                    tile = getTileWithId(tileID, self.game.ALL_TILES)
+                    if tile is not None:
+                        tile.draw(screen, x, y)
+                """decorID = getTile(x, y, decorations)
+                if decorID is not None:
+                    decor = getTileWithId(decorID, ALL_DECORATIONS)
+                    if decor is not None:
+                        decor.draw(screen, x, y)"""
 
-    text = font.render('DT: '+ str(dt), True, (255, 255, 255))
-    screen.blit(text, (10, 50))
+        
 
-    """
-    text = font.render('playerX: '+ str(player.x) + '  (' + str(player.x//TILE_SIZE) + ')', True, (255, 255, 255))
-    screen.blit(text, (10, 90))
+        self.player.draw(screen)
 
-    text = font.render('playerY: '+ str(player.y) + '  (' + str(player.y//TILE_SIZE) + ')', True, (255, 255, 255))
-    screen.blit(text, (10, 130))
+        time = pygame.time.get_ticks() / 1000
+        text = self.game.font.render('Tid: ' + str(time), True, (255, 255, 255))
+        screen.blit(text, (10, 10))
 
-    text = font.render('vx: '+ str(player.vx), True, (255, 255, 255))
-    screen.blit(text, (10, 170))
+        text = self.game.font.render('DT: '+ str(dt), True, (255, 255, 255))
+        screen.blit(text, (10, 50))
 
-    text = font.render('vy: '+ str(player.vy), True, (255, 255, 255))
-    screen.blit(text, (10, 210))
 
-    text = font.render('grounded: '+ str(player.grounded), True, (255, 255, 255))
-    screen.blit(text, (10, 250))
-    """
-
-    pygame.display.flip()
-
-pygame.quit()
+game = Game()
+game.run()
